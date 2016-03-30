a38f2dee8954be8e99993aea47c50144bdc54d99
Johnny So <jso123450@gmail.com>
2016-03-29 20:56:52 -0400

    added some stuff

--- d3.js
+++ d3.js
@@ -2,21 +2,22 @@
 console.log("Javascript loaded!");
 
 /* ---------------------- D3 BABY ---------------------- */
+
+// Bar Graph
+
+// The initial variable setup
 var margin_top = 50;
 var margin_bottom = 50;
 var margin_right = 50;
 var margin_left = 50;
 
-var width = 700 - margin_left - margin_right;
-var height = 350 - margin_top - margin_bottom;
+var bar_width = 700 - margin_left - margin_right;
+var bar_height = 350 - margin_top - margin_bottom;
 
-var x = d3.scale.ordinal().rangeRoundBands( [0,width], .05);
-var y = d3.scale.linear().range([height,0]);
-
+// Formatting the axes
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m"));
+    .orient("bottom");
 
 var yAxis = d3.svg.axis()
     .scale(y)
@@ -24,24 +25,27 @@ var yAxis = d3.svg.axis()
     .ticks(10);
 
 var svg = d3.select("body").append("svg")
-    .attr("width", margin_left + width + margin_right)
-    .attr("height", margin_bottom + height + margin_top)
+    .attr("width", margin_left + bar_width + margin_right)
+    .attr("height", margin_bottom + bar_height + margin_top)
     .append("g")
     .attr("transform", "translate(" + margin_left + "," + margin_top + ")");
 
-d3.csv("bar-data.csv", function(error, data) {
+d3.csv("poll_res.csv", function(error, data) {
 
     data.forEach(function(d) {
-        d.date = parseDate(d.date);
+        d.name = d.name;
         d.value = +d.value;
     });
-	
-  x.domain(data.map(function(d) { return d.date; }));
+    	
+    // Create the domains for each axis
+    // the x axis will just be each name
+    // the y axis will be the number of votes
+  x.domain(data.map(function(d) { return d.name; }));
   y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
+    
   svg.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
+      .attr("transform", "translate(0," + bar_height + ")")
       .call(xAxis)
     .selectAll("text")
       .style("text-anchor", "end")
@@ -63,10 +67,59 @@ d3.csv("bar-data.csv", function(error, data) {
       .data(data)
     .enter().append("rect")
       .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.date); })
+      .attr("x", function(d) { return x(d.name); })
       .attr("width", x.rangeBand())
       .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
+      .attr("height", function(d) { return bar_height - y(d.value); });
 
 });
 
+// Pie Chart
+
+var pie_width = 960,
+var pie_height = 500,
+var radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 15)
+    .innerRadius(0);
+
+var labelArc = d3.svg.arc()
+    .outerRadius(radius - 30)
+    .innerRadius(radius - 30);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.value; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", pie_width)
+    .attr("height", pie_height)
+  .append("g")
+    .attr("transform", "translate(" + pie_width / 2 + "," + pie_height / 2 + ")");
+
+d3.csv("data.csv", type, function(error, data) {
+  if (error) throw error;
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+    .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.age); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function(d) { return d.data.age; });
+});
+
+function type(d) {
+  d.population = +d.population;
+  return d;
+}
